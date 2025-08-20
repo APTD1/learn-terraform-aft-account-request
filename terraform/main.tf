53379ce6af08e5ad0cc3470c6e40fc963da4f535
Afeez Azeez <azeezafeez119@gmail.com>
2025-08-20 12:04:44 +0100

    create a sandbox account using AFT

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = ""
+    AccountEmail              = "azeezafeez119+ooshlink@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = ""
+    SSOUserEmail              = "azeezafeez119+ooshlink@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
