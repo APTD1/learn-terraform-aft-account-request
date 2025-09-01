79330e8dd7c6bbcdbf8f74d0e4cee0cadf4b1771
Afeez Azeez <azeezafeez119@gmail.com>
2025-09-01 11:30:01 +0100

    New account for Essential

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "azeezafeez119+essential2@gmail.com"
-    AccountName               = "Afeez-aft"
+    AccountEmail              = "azeezafeez119+essential1@gmail.com"
+    AccountName               = "Sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "azeezafeez119+essential2@gmail.com"
+    SSOUserEmail              = "azeezafeez119+essential1@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
@@ -15,8 +15,37 @@ module "sandbox" {
   }
 
   change_management_parameters = {
-    change_requested_by = "Essential Nigeria"
-    change_reason       = "New account creation"
+    change_requested_by = "Essential Group"
+    change_reason       = "Account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "azeezafeez119+essential2@gmail.com"
+    AccountName               = "Essential-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "azeezafeez119@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Essential Group"
+    change_reason       = "New account creation 1"
   }
 
   custom_fields = {
